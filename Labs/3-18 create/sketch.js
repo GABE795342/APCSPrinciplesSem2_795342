ecbf55b7852c791ae38ea21a79adfb511727336f
GABE795342 <795342@seq.org>
2019-03-19 08:47:46 -0700

    3-19 work
    3-19 work

---
+++ Labs/3-18 create/sketch.js
@@ -0,0 +1,54 @@
+// // https://docs.google.com/presentation/d/1UnMJ2NWzQ4_Qo7lftdEj7KPZgQaapINQcbj1Z3Vjps0/edit?usp=sharing
+// http://api.open-notify.org/astros.json
+
+var jsonfilevar;
+
+var men;
+var women;
+
+
+
+
+function preload(){
+	jsonfilevar = loadJSON("usgovernors.json");
+}
+function setup() {
+
+	console.log(jsonfilevar);
+}
+
+function draw (){
+	
+	
+	
+	
+	
+}
+
+
+function gendersort(){
+	for(var i = 0; i < 50; i++){
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
