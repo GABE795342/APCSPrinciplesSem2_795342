e57bc4f199a3a60ad6d90b4562e648d318d57d44
GABE795342 <795342@seq.org>
2019-05-02 12:09:23 -0700

    5-2 last ditch 2
    5-2 last ditch 2 almost done failing

--- Labs/3-18 create/sketch.js
+++ Labs/3-18 create/sketch.js
@@ -1,58 +1,49 @@
-// current people in space JSON http://api.open-notify.org/astros.json
-                                       
 var jsonfilevar;
-var data;
 var StateGovernors;
-var jsonarrayobject;
-var jsondataretrive;
+//var jsondataretrive;
 var republicancount; //number of Republican governors
 var democratcount; //number of Democrat governors
 var i;
 
 
 
+
 function preload(){
-	data = loadJSON("usgovernors.json");
+	jsonfilevar = loadJSON("usgovernors.json");
 	//StateGovernors = loadJSON("usgovernors.json");
+
+
 }
 
-
-
-
 function setup() {
-    var cnv = createCanvas(800, 800);
-    cnv.position((windowWidth-width)/2, 30);
-    background(200);
-
-
-
-
-	console.log(data);
+  //GRAPHICS UNNECCESARY
+  //  var cnv = createCanvas(800, 800);
+  //  cnv.position((windowWidth-width)/2, 30);
+  //  background(200);
+	console.log(jsonfilevar);
 	new GeneralJSONArrayLister();
 //	let jsondataretrive = ( );
 }
-
-function BarGraph(sequencenumber, valuenumber){
-	rect((150),50,xvaluenumber,50)
-}
+//UNUSED BARGRAPH FUNCTION
+//function BarGraph(sequencenumber, valuenumber){
+//	rect((150),50,xvaluenumber,50);
+//}
 
 function GeneralJSONArrayLister(){
-	var republicancount = 0; 
-	var democratcount = 0; 
-	
-	for(var i = 1; i < 51; i++){   //for loop increments 50x, one for each state
-		if (data.StateGovernors[i].party = "republican") {
+	var republicancount = 0;
+	var democratcount = 0;
+
+	for(var i = 0; i < 50; i++){   //for loop increments 50x, one for each state
+		if (jsonfilevar.StateGovernors[i].party = "republican") {
 			republicancount++;
 			//console.log(republicancount);
-		} else {
-		
-		democratcount++;
-		//console.log(democratcount);
 		}
-		
-	
-	
+		//original else won't work for if they are independent governors (none currently) they will be counted as democrats which is innacurate
+		//else {
 
+		//democratcount++;
+		//console.log(democratcount);
+		//}
 	}
 	console.log(republicancount)
 	console.log(democratcount);
